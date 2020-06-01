f5ca60cf32d2ccffb7283995702533ce38f2ea38
Juliane Schremer <juliane.schremer@gmail.com>
2020-06-01 16:19:49 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_142745) do
+ActiveRecord::Schema.define(version: 2020_05_22_145136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,16 +40,14 @@ ActiveRecord::Schema.define(version: 2020_05_24_142745) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "url"
-    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
-    t.text "description"
     t.bigint "cocktail_id", null: false
     t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
