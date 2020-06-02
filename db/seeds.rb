a5e712fb46510014023becfe6d8de319377c56e0
Juliane Schremer <juliane.schremer@gmail.com>
2020-06-02 13:00:05 +0200

    Restaurants done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Restaurant 1", address: "Street 1", city: "Berlin", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 2", address: "Street 2", city: "Berlin", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 3", address: "Street 3", city: "Berlin", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 4", address: "Street 4", city: "Hamburg", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 5", address: "Street 4", city: "Hamburg", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 6", address: "Street 4", city: "Hamburg", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 8", address: "Street 4", city: "Stuttgart", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 9", address: "Street 4", city: "Stuttgart", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 10", address: "Street 4", city: "Stuttgart", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 11", address: "Street 4", city: "Dortmund", description: "awesome place with great food")
+
+Restaurant.create(name: "Restaurant 12", address: "Street 4", city: "Dortmund", description: "awesome 
+place with great food")
+
+Restaurant.create(name: "Restaurant 13", address: "Street 4", city: "Dortmund", description: "awesome place with great food")
+
