f23c22bc3eef37862cf819608f8931a19b50f245
Daniela Gordas <daniela.gordas@gmail.com>
2020-06-03 16:03:20 +0200

    shopping cart logic

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,14 +3,22 @@ class OrderItemsController < ApplicationController
   def new
     @order_item = OrderItem.new
     @meal = Meal.find(params[:meal_id])
-    @order = Order.create(user: current_user, restaurant: @meal.restaurant)
+    if current_user.orders.where(paid: false).first
+      @order = current_user.orders.where(paid: false).first
+    else
+      @order = Order.create(user: current_user, restaurant: @meal.restaurant)
+    end
   end
 
   def create
     @meal = Meal.find(params[:meal_id])
     @order_item = OrderItem.new(order_item_params)
     @order_item.meal = @meal
-    @order = Order.create!(user: current_user, restaurant: @meal.restaurant)
+    if current_user.orders.where(paid: false).first
+      @order = current_user.orders.where(paid: false).first
+    else
+      @order = Order.create!(user: current_user, restaurant: @meal.restaurant)
+    end
     @order_item.order = @order
     if @order_item.save
       redirect_to meal_path(@meal), notice: 'Booking was successfully created!'
