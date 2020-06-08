79d3f28cf860ef0696d68bbdccd4e8eb8fa7fcd0
Daniela Gordas <daniela.gordas@gmail.com>
2020-06-08 18:34:02 +0200

    fixed current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :set_counter
   before_action :current_order
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -11,15 +11,19 @@ class ApplicationController < ActionController::Base
   private
 
   def set_counter
-    @order_items_count = 0
-    @order = current_user.orders.where(paid: false).first
-    if @order
-      @order_items_count = @order.order_items.count
+    if current_user
+      @order_items_count = 0
+      @order = current_user.orders.where(paid: false).first
+      if @order
+        @order_items_count = @order.order_items.count
+      end
     end
   end
 
   def current_order
-    @order = current_user.orders.where(paid: false).first
+    if current_user
+      @order = current_user.orders.where(paid: false).first
+    end
   end
 
 end
