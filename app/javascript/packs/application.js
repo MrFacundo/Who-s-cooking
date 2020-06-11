bd17e7c3acee8cb18257854988e8b708c401f2cf
Daniela Gordas <daniela.gordas@gmail.com>
2020-06-11 12:06:36 +0200

    added flatpickr for the date and time

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
-
+import "../plugins/flatpickr.js"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,14 +20,13 @@ import 'bootstrap';
 
 import "controllers";
 import { initAutocomplete } from '../plugins/init_autocomplete';
-import { initInput } from '../components/card.js'; 
+import { initInput } from '../components/card.js';
 
 $(document).ready(function(){
   setTimeout(function(){
   $('.alert').fadeOut();
-  }, 4000);
+  }, 3000);
 });
-
 
 
 document.addEventListener('turbolinks:load', () => {
